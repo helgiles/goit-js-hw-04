caef1eececc6e568521a511e71410bebf570bdf9
Олег Слюсар <eternityonly1@gmail.com>
2023-11-05 09:49:45 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -5,7 +5,7 @@ const profile = {
 		this.username = newName;
 	},
 	updatePlayTime(hours) {
-		this.playTime = hours;
+		this.playTime += hours;
 	},
 	getInfo() {
 		return `${this.username} has ${this.playTime} active hours!`;
@@ -17,5 +17,5 @@ console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 profile.changeUsername('Marco');
 console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
-profile.updatePlayTime(320);
+profile.updatePlayTime(20);
 console.log(profile.getInfo()); // "Marco has 320 active hours!"
